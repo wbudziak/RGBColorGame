bc53694bb72dd2c88f5bc940465bb70f4ece87e4
Wojciech Budziak <steamvexlex@gmail.com>
2021-03-09 14:34:29 +0100

    .

--- main.js
+++ main.js
@@ -3,35 +3,41 @@ const easyMode = document.querySelector('.easy');
 const hardMode = document.querySelector('.hard');
 const rgb = document.querySelector('.rgb');
 const header = document.querySelector('header');
+
 let howManyTiles = 6;
 let randomColors = [];
 let tiles = [];
 let timeToAnimation = 50;
-const resetData = ()=>{
+
+const resetData = () => {
     timeToAnimation = 50;
     colorContainer.innerHTML = "";
     randomColors = [];
-    header.style.backgroundColor = "rgb(198, 195, 195)";
+    // header.style.backgroundColor = "rgb(198, 195, 195)";
     createTile();
 }
+
 easyMode.addEventListener('click', () => {
     howManyTiles = 3;
     resetData();
 })
+
 hardMode.addEventListener('click', () => {
     howManyTiles = 6;
     resetData();
 })
 
-const checkResult = (randomID,randomTile2)=>{
+const checkResult = (randomID, randomTile2) => {
     tiles = [...document.querySelectorAll(".color-container__box")];
     tiles.forEach(tile => {
         tile.addEventListener('click', () => {
             if (tile.style.backgroundColor === randomTile2) {
                 alert("zgadłeś!");
-                header.style.backgroundColor = randomTile2;
+                tiles.forEach(tile => {
+                    tile.style.backgroundColor = randomTile2;
+                });
+                // header.style.backgroundColor = randomTile2;
                 setTimeout(() => {
-                    header.style.backgroundColor = "rgb(198, 195, 195)";
                     resetData();
                 }, 1000);
             } else {
@@ -40,18 +46,19 @@ const checkResult = (randomID,randomTile2)=>{
         })
     });
 }
+
 const randomTile = () => {
     const randomID = Math.floor(Math.random() * randomColors.length);
     const randomTile2 = randomColors[randomID];
     rgb.textContent = randomTile2;
-    checkResult(randomID,randomTile2);
+    checkResult(randomID, randomTile2);
 }
 
 const createTile = () => {
     for (let i = 0; i < howManyTiles; i++) {
         timeToAnimation += 50;
         setTimeout(() => {
-         tile.style.opacity = "1";
+            tile.style.opacity = "1";
         }, timeToAnimation);
         let randomColor1 = Math.floor(Math.random() * 255);
         let randomColor2 = Math.floor(Math.random() * 255);
